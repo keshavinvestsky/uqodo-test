e6864f11a71ef24343f28292a5e3d5c334d16d4b
Keshav Katwe <keshav@investsky.com>
2024-10-30 14:59:41 +0400

    added navigation

--- App.tsx
+++ App.tsx
@@ -5,114 +5,130 @@
  * @format
  */
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
+import React from "react";
+import type { PropsWithChildren } from "react";
 import {
+  Button,
   SafeAreaView,
   ScrollView,
   StatusBar,
   StyleSheet,
-  Text,
+  Text, TouchableOpacity,
   useColorScheme,
-  View,
-} from 'react-native';
+  View
+} from "react-native";
 
 import {
   Colors,
   DebugInstructions,
   Header,
   LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+  ReloadInstructions
+} from "react-native/Libraries/NewAppScreen";
+import {
+  AppearanceMode,
+  DocumentBuilder,
+  DocumentType,
+  EnrollmentBuilder,
+  ReadingConfigurationBuilder,
+  UqudoIdSDK
+} from "uqudosdk-react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "react-native-screens/native";
 
 type SectionProps = PropsWithChildren<{
   title: string;
 }>;
 
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+const uqudoInstance = new UqudoIdSDK();
+uqudoInstance.init();
+
+function HomeScreen({ children, title }: SectionProps): React.JSX.Element {
+  const isDarkMode = useColorScheme() === "dark";
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
+    <SafeAreaView>
+      <View style={styles.sectionContainer}>
+        <TouchableOpacity
+          onPress={async () => {
+            try {
+              const token = "TOKEN";
+              let documentBuilder = new DocumentBuilder().setDocumentType(
+                "UAE_ID"
+              );
+
+              let enrollmentConfiguration = new EnrollmentBuilder()
+                .setToken(token)
+                .enableFacialRecognition()
+                .add(documentBuilder.build())
+                .setAppearanceMode(AppearanceMode.SYSTEM)
+                .build();
+
+              await uqudoInstance.enroll(enrollmentConfiguration);
+            } catch (e) {
+              console.log(e);
+            }
+          }}>
+          <Text>Hello</Text>
+        </TouchableOpacity>
+        <Text
+          style={[
+            styles.sectionTitle,
+            {
+              color: isDarkMode ? Colors.white : Colors.black
+            }
+          ]}>
+          {title}
+        </Text>
+        <Text
+          style={[
+            styles.sectionDescription,
+            {
+              color: isDarkMode ? Colors.light : Colors.dark
+            }
+          ]}>
+          {children}
+        </Text>
+      </View>
+    </SafeAreaView>
   );
 }
 
+
+const Stack = createStackNavigator();
+
 function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+  const isDarkMode = useColorScheme() === "dark";
 
   const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter
   };
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   sectionContainer: {
     marginTop: 32,
-    paddingHorizontal: 24,
+    paddingHorizontal: 24
   },
   sectionTitle: {
     fontSize: 24,
-    fontWeight: '600',
+    fontWeight: "600"
   },
   sectionDescription: {
     marginTop: 8,
     fontSize: 18,
-    fontWeight: '400',
+    fontWeight: "400"
   },
   highlight: {
-    fontWeight: '700',
-  },
+    fontWeight: "700"
+  }
 });
 
 export default App;
